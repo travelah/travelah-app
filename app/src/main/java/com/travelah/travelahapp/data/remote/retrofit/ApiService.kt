50658532b6c64da99cb7edc9112cdcfec97c1c0f
Zuhal 'Alimul Hadi <74417769+zuhalal@users.noreply.github.com>
2023-05-30 22:21:23 +0700

    fix: login and profile response (#2)

--- app/src/main/java/com/travelah/travelahapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/travelah/travelahapp/data/remote/retrofit/ApiService.kt
@@ -1,15 +1,15 @@
 package com.travelah.travelahapp.data.remote.retrofit
 
-import com.travelah.travelahapp.data.remote.models.AuthResponse
-import com.travelah.travelahapp.data.remote.models.body.LoginBody
+import com.travelah.travelahapp.data.remote.models.LoginResponse
 import com.travelah.travelahapp.data.remote.models.ProfileResponse
+import com.travelah.travelahapp.data.remote.models.body.LoginBody
 import retrofit2.http.*
 
 interface ApiService {
     @POST("auth/login")
     suspend fun login(
         @Body body: LoginBody
-    ): AuthResponse
+    ): LoginResponse
 
     @GET("users/profile")
     suspend fun profile(
