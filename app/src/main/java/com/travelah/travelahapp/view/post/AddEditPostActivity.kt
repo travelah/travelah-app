40f54f51d5a7818bb92d79063affa2e8c818dfa1
zuhalal <zuhalalimulhadi19@gmail.com>
2023-06-07 21:37:30 +0700

    feat: add edit post layout

---
+++ app/src/main/java/com/travelah/travelahapp/view/post/AddEditPostActivity.kt
@@ -0,0 +1,12 @@
+package com.travelah.travelahapp.view.post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.travelah.travelahapp.R
+
+class AddEditPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_post)
+    }
+}
