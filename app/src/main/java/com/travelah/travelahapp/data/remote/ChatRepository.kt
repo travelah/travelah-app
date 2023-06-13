83998c072b2d0c3093efcd6784207133252e64f6
zuhalal <zuhalalimulhadi19@gmail.com>
2023-06-13 21:37:21 +0700

    Revert "feat: try my best to implement with paging3 but not realtime when submit"
    This reverts commit 7bee73972fdae3bcdeba9c8cb1fa085e17163ca6.

--- app/src/main/java/com/travelah/travelahapp/data/remote/ChatRepository.kt
+++ app/src/main/java/com/travelah/travelahapp/data/remote/ChatRepository.kt
@@ -7,16 +7,11 @@ import com.google.gson.Gson
 import com.travelah.travelahapp.data.Result
 import com.travelah.travelahapp.data.local.entity.ChatEntity
 import com.travelah.travelahapp.data.local.room.TravelahDatabase
-import com.travelah.travelahapp.data.remote.models.Comment
 import com.travelah.travelahapp.data.remote.models.ErrorResponse
 import com.travelah.travelahapp.data.remote.models.HistoryChat
-import com.travelah.travelahapp.data.remote.models.response.ChatItem
-import com.travelah.travelahapp.data.remote.pager.ChatListPagingSource
 import com.travelah.travelahapp.data.remote.pager.ChatRemoteMediator
-import com.travelah.travelahapp.data.remote.pager.PostCommentPagingSource
 import com.travelah.travelahapp.data.remote.retrofit.ApiService
 import com.travelah.travelahapp.utils.wrapEspressoIdlingResource
-import kotlinx.coroutines.flow.Flow
 import retrofit2.HttpException
 
 class ChatRepository private constructor(
@@ -66,6 +61,8 @@ class ChatRepository private constructor(
         ).liveData
     }
 
+
+
     fun deleteGroupChat(token: String, id: Int) : LiveData<Result<String>> = liveData {
         emit(Result.Loading)
 
@@ -90,18 +87,6 @@ class ChatRepository private constructor(
                 }
             }
         }
-    }
-
-    fun getAllChatInGroup(groupId: Int, token: String): Flow<PagingData<ChatItem>> {
-        return Pager(
-            config = PagingConfig(
-                pageSize = 5,
-                initialLoadSize = 15
-            ),
-            pagingSourceFactory = {
-                ChatListPagingSource(groupId, token)
-            }
-        ).flow
     }
 
     companion object {
